38faf3eda215459538f8652ccf3558efcef64094
Antonis Loizou <a.loizou@vu.nl>
2015-07-28 22:35:38 +0200

    Moved header - part 1

--- wordpress/wp-content/themes/inhopp/front-page.php
+++ wordpress/wp-content/themes/inhopp/front-page.php
@@ -9,7 +9,7 @@
  * @since Voss Inhopp 1.0
  */
 
-get_header(); ?>
+get_template_part('header', 'front-page'); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
